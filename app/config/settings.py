8319e090c21f2ffc5fabf76b46e6db0ac6ec4d1f
Pavel Korolev <p.koror56@gmail.com>
2025-11-12 13:12:46 +0500

    Add app/config/settings.py to load .env with pydantic_settings.BaseSettings

---
+++ app/config/settings.py
@@ -0,0 +1,22 @@
+from pydantic import ValidationError
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Глобальные настройки."""
+
+    DATABASE_URL: str = ''
+    DATABASE_URL_ASYNC: str = ''
+    ENV: str = "dev"
+    DEBUG: bool = True
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        extra = "ignore"
+
+
+try:
+    settings = Settings()
+except ValidationError as e:
+    raise SystemExit(f"Ошибка конфигурации:\n{e}")
